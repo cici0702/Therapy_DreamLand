42d9765d48da249bfa2601cc8523b5888e5e5127
cici0702 <qdua0005@student.monash.edu>
2022-09-17 17:43:06 +1000

    create project

---
+++ Therapy/Therapy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Therapy.Startup))]
+namespace Therapy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
